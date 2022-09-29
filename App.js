94aec379094af7ba20167cf9c68f7f47c269e672
Jiyeon <71193553+jiyeonCoder@users.noreply.github.com>
2022-09-29 18:14:43 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+//import logo from "./logo.svg";
+import React, { useEffect } from "react";
+import "./App.css";
+import Start from "./Start";
+
+function App() {
+  const [name, setName] = React.useState("Jiyeon");
+
+  return (
+    <div
+      className="App"
+      style={{
+        maxWidth: "350px",
+        margin: "auto",
+      }}>
+      <Start name={name} />
+    </div>
+  );
+}
+
+export default App;
