2c47e320e20fde6cecedbf011d9837627518bea3
Jiyeon <71193553+jiyeonCoder@users.noreply.github.com>
2022-09-30 17:07:39 +0900

    Add files via upload

---
+++ Text.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Text = (props) => {
+  const text = React.useRef(null);
+
+  const hoverEvent = () => {
+    text.current.style.background = "yellow";
+  };
+  React.useEffect(() => {
+    text.current.addEventListener("mouseover", hoverEvent);
+
+    //Clean up at return
+    return () => {
+      text.current.removeEventListener("mouseover", hoverEvent);
+    };
+  }, []);
+  return <h1 ref={text}>텍스트입니다!</h1>;
+};
+
+export default Text;
