e6fffadd2875de900fcfa7a396067b1b7c910fb4
Jiyeon <71193553+jiyeonCoder@users.noreply.github.com>
2022-09-30 17:04:57 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-//import logo from "./logo.svg";
-import React, { useEffect } from "react";
+import logo from "./logo.svg";
 import "./App.css";
-import Start from "./Start";
+import React from "react";
+import Text from "./Text";
 
-function App() {
-  const [name, setName] = React.useState("Jiyeon");
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div
-      className="App"
-      style={{
-        maxWidth: "350px",
-        margin: "auto",
-      }}>
-      <Start name={name} />
-    </div>
-  );
+    this.state = {};
+    this.circle = React.createRef(null);
+  }
+
+  hoverEvent = (e) => {
+    console.log(e.target);
+    console.log(this.circle.current);
+    this.circle.current.style.background = "yellow";
+  };
+
+  componentDidMount() {
+    console.log(this.circle);
+    this.circle.current.addEventListener("mouseover", this.hoverEvent);
+  }
+
+  componentWillUnmount() {
+    this.circle.current.removeEventListener("mouseover", this.hoverEvent);
+  }
+
+  render() {
+    return (
+      <div style={{ width: "100vw", height: "100vh", textAlign: "center" }}>
+        <Text />
+        <div
+          style={{
+            margin: "auto",
+            width: "250px",
+            height: "250px",
+            background: "green",
+            borderRadius: "250px",
+          }}
+          ref={this.circle}></div>
+      </div>
+    );
+  }
 }
 
 export default App;
