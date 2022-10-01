83c6a5da42bfce53a855d97967f93119754856de
Jiyeon <71193553+jiyeonCoder@users.noreply.github.com>
2022-10-01 20:22:49 +0900

    Create Cat.js

---
+++ Cat.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import { useParams } from "react-router-dom";
+
+const Cat = (props) => {
+  //const cat_name = useParams();
+  console.log(props);
+  return <div>고양이 화면입니다!</div>;
+};
+
+export default Cat;
